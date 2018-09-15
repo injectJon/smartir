f1a0d854551af4a921517d6e9530ce9f4449afb9
injectJon <injectJon@gmail.com>
2018-09-14 19:57:56 -0500

    Home page should return a html file.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ mongoose.connect( process.env.MONGODB_URI );
 const app = express();
 
 app.get( '/', ( req, res ) => {
+    res.sendFile('index.html', {root: __dirname })
+} );
+
+app.get( '/test', ( req, res ) => {
     res.json( { status: 200, message: 'This is the home endpoint!' } );
 } );
 
